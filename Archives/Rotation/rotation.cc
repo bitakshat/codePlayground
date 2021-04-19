1de7f745dfd3effcb96e525817360001ee6bbad1
bitakshat <akshatpal1232@gmail.com>
2021-04-19 14:22:38 +0530

    [+] Generates random array

--- Archives/Rotation/rotation.cc
+++ Archives/Rotation/rotation.cc
@@ -1,33 +1,48 @@
 
 #include <iostream>
+#include <random>
 
 // Rotate by RB
-int *_rotate(int *arr, int RB, int size)
+int *rotate(int *arr, int RB, int size)
 {
-    for (int i = 0; i < RB; ++i)
-    {
+    for ( int i = 0; i < RB; ++i ) {
         int temp = arr[0];
-        for (int x = 0; x < size - 1; x++)
-        {
+        for ( int x = 0; x < size - 1; x++ ) {
             arr[x] = arr[x + 1];
         }
         arr[size - 1] = temp;
     }
-
     return arr;
 }
 
 int main()
-{
-    int RB = 3;
-    int arr[] = {3, 2, 1, 5, 4};
-    int size = sizeof(arr) / sizeof(arr[0]);
- 
-    /************ function call **************/
-    int *newArr = _rotate(arr, RB, size);
-    for (int j = 0; j <= size - 1; j++)
-        std::cout << *(newArr + j) << " ";
+{      
+    std::random_device r;
+    std::mt19937 mt1(r());
+    std::uniform_int_distribution<int> d1(5, 10);
+    int range = d1(r);
 
+    // RB value can be changed by user
+    int RB = 1;
+    int arr[] = {};
+    
+    for( int x = 0; x < range; x++ ) {
+        std::random_device v;
+        std::mt19937 mt2(v());
+        std::uniform_int_distribution<int> d2(0, 9);
+        arr[x] = d2(mt2);
+    }
+
+    std::cout << "initial array: " << std::endl;
+    for( int j = 0; j < range; j++ ) {
+        std::cout << arr[j] << " ";
+    }
+    
+    /**************** function call *****************/
+    std::cout << std::endl << "array after rotation: " << std::endl;
+    int *newArr = rotate(arr, RB, range);
+    for (int j = 0; j <= range - 1; j++)
+        std::cout << *(newArr + j) << " ";
+    
     return 0;
 }
-
