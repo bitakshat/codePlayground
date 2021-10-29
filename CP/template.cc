fb78583d767006113f6848d612cd159c2bfaabef
bitakshat <akshatpal1232@gmail.com>
2021-10-29 19:41:48 +0530

    [+] add new template file for CP

---
+++ CP/template.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#define lint long long int
+#define itr(a, b) for (int a = 0; a < b; a++)
+#define dbg(var) std::cout << (#var) << ": " << var << '\n'
+
+
+int main(void) {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    return 0;
+}
+
